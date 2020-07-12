3e40d0d6ea85d12905136cfb9c923f84f72b9377
Seungjae Han <henry7646@gmail.com>
2020-07-12 15:42:57 +0900

    Updated 운동기록.R and 운동기록.png
    Added horizontal lines indicating my target WHR and target WHtR with text+arrow annotations

--- exercise/운동기록.R
+++ exercise/운동기록.R
@@ -1,4 +1,5 @@
 getwd()
+setwd("C:/Users/Seung Jae Han/Dropbox/Trivia/øÓµø±‚∑œ")
 .libPaths()
 rm(list = ls())
 #install.packages("dplyr")
@@ -9,6 +10,7 @@ rm(list = ls())
 #install.packages("writexl")
 #install.packages("ggpubr")
 #install.packages("tidyverse")
+#install.packages("ggthemes")
 library(readr)
 library(readxl)
 library(dplyr)
@@ -17,20 +19,43 @@ library(lubridate)
 library(ggplot2)
 library(ggpubr)
 
-exercise <- read_xlsx("Ïö¥ÎèôÍ∏∞Î°ù.xlsx")
+exercise <- read_xlsx("C:/Users/Seung Jae Han/Dropbox/Trivia/øÓµø±‚∑œ/øÓµø±‚∑œ.xlsx")
 str(exercise$DATETIME)
 
 whr <- exercise %>%
-  ggplot(aes(as.Date(DATETIME),WHR))+geom_line()+scale_x_date("DATE",date_breaks = "1 day", date_labels = "%y/%m/%d")+scale_y_continuous(limits=c(0.7,1),breaks=seq(0.7,1,0.1),expand=c(0,0))+theme(axis.text.x = element_text(angle = 45))
+  ggplot(aes(as.Date(DATETIME),WHR))+geom_line()+
+  scale_x_date("DATE",date_breaks = "1 day", date_labels = "%y/%m/%d")+
+  scale_y_continuous(limits=c(0.7,1),breaks=seq(0.7,1,0.1),expand=c(0,0))+
+  geom_hline(yintercept = 0.80, color = "blue", linetype = 5)+
+  annotate("text", x = ymd("2020/06/23"), y = 0.78, 
+           label = "Target\nWHR",vjust = 1, size = 3, color = "grey40")+
+  annotate("curve",x = ymd("2020/06/23"), y = 0.78, 
+           xend = ymd("2020/06/23"), yend = 0.795,
+           arrow = arrow(length=unit(0.15,"cm"), type = "closed"),
+           color = "grey40")+
+  theme(axis.text.x = element_text(angle = 45), 
+        panel.grid.minor.x = element_blank())
 whr
 
 whtr <- exercise %>%
-  ggplot(aes(as.Date(DATETIME),WHTR))+geom_line()+scale_x_date("DATE",date_breaks = "1 day", date_labels = "%y/%m/%d")+scale_y_continuous(limits=c(0.4,0.7),breaks=seq(0.4,0.7,0.1),expand=c(0,0))+theme(axis.text.x = element_text(angle = 45))
+  ggplot(aes(as.Date(DATETIME),WHTR))+geom_line()+
+  scale_x_date("DATE",date_breaks = "1 day", date_labels = "%y/%m/%d")+
+  scale_y_continuous(limits=c(0.4,0.7),breaks=seq(0.4,0.7,0.1),
+                     expand=c(0,0))+
+  geom_hline(yintercept = 0.50, color = "blue", linetype = 5)+
+  annotate("text", x = ymd("2020/06/23"), y = 0.48, 
+           label = "Target\nWHtR",vjust = 1, size = 3, color = "grey40")+
+  annotate("curve",x = ymd("2020/06/23"), y = 0.48, 
+           xend = ymd("2020/06/23"), yend = 0.495,
+           arrow = arrow(length=unit(0.15,"cm"), type = "closed"),
+           color = "grey40")+
+  theme(axis.text.x = element_text(angle = 45),
+        panel.grid.minor.x = element_blank())
 whtr
 
 whr_whtr <- ggarrange(whr, whtr, ncol = 1, nrow = 2, heights = c(2,2), align = "v")
 whr_whtr
 
-Ïö¥ÎèôÍ∏∞Î°ù <- whr_whtr %>%
-  annotate_figure(top = text_grob("Ïö¥ÎèôÍ∏∞Î°ù",face = "bold", size = 14))
-Ïö¥ÎèôÍ∏∞Î°ù
+øÓµø±‚∑œ <- whr_whtr %>%
+  annotate_figure(top = text_grob("øÓµø±‚∑œ",face = "bold", size = 14))
+øÓµø±‚∑œ
