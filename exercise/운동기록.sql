a096f5664d6c55c17d73d7ee28c94f9a5e683ff9
Seungjae Han <henry7646@gmail.com>
2020-08-03 11:37:23 +0900

    Updated 운동기록.png and 운동기록.sql

--- exercise/운동기록.sql
+++ exercise/운동기록.sql
@@ -69,7 +69,7 @@ variable 조회결과 varchar2;
 variable 허리엉덩이비율 number;
 variable 허리신장비율 number;
 variable 체질량지수 number;
-EXECUTE whr_update(TO_DATE(SYSDATE,'YY/MM/DD'),96.4,102.2,81.0,:조회결과,:허리엉덩이비율,:허리신장비율,:체질량지수);
+EXECUTE whr_update(TO_DATE(SYSDATE,'YY/MM/DD'),95.7,101.8,79.5,:조회결과,:허리엉덩이비율,:허리신장비율,:체질량지수);
 PRINT 조회결과;
 PRINT 허리엉덩이비율;
 PRINT 허리신장비율;
